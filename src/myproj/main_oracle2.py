51d3d34c9216c9c3ffea9e3f7c7c560c3dbac431
lsj <lsjjsl004@naver.com>
2025-06-17 17:20:05 +0900

    update

---
+++ src/myproj/main_oracle2.py
@@ -0,0 +1,16 @@
+import oracledb
+
+user = 'Scott'
+password = 'tiger'
+dsn = 'localhost/xe'
+
+if __name__ == '__main__':
+    # with-as 구문을 사용한 DB 접속, Cursor 사용.
+    with oracledb.connect(user = user, password = password, dsn = dsn) as conn:
+        # conn.close() 메서드 호출은 with-as가 끝날 때 자동으로 호출됨.
+        with conn.cursor() as cursor:
+            # cursor.close() 메서드 호출은 with-as가 끝날 때 자동으로 호출됨.
+            sql = 'select * from dept'
+            cursor.execute(sql)
+            for row in cursor:
+                print(row)
